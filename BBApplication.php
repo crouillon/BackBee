c0e04d512785b6f6fe90a9d0a81f54114a9637ec
Charles Rouillon <charles.rouillon@lp-digital.fr>
2017-05-31 11:31:01 +0200

    Update version to v1.3.0

--- BBApplication.php
+++ BBApplication.php
@@ -55,7 +55,7 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
  */
 class BBApplication implements ApplicationInterface, DumpableServiceInterface, DumpableServiceProxyInterface
 {
-    const VERSION = '1.2.10-DEV';
+    const VERSION = '1.3.0';
 
     /**
      * application's service container.
