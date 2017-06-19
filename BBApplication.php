c28dbde8c78a115a3845a01eb7254c0ce7cde13f
Charles Rouillon <charles.rouillon@lp-digital.fr>
2017-06-19 16:55:40 +0200

    Prepare release v1.3.3

--- BBApplication.php
+++ BBApplication.php
@@ -55,7 +55,7 @@ use Symfony\Component\HttpKernel\HttpKernelInterface;
  */
 class BBApplication implements ApplicationInterface, DumpableServiceInterface, DumpableServiceProxyInterface
 {
-    const VERSION = '1.3.3-DEV';
+    const VERSION = '1.3.3';
 
     /**
      * application's service container.
