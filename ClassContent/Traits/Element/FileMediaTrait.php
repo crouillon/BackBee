1725ccb4889dacde714d9387d2f6f87395ac3b28
Djoudi Bensid <djoudi.bensid@lp-digital.fr>
2017-12-21 12:26:09 +0100

    [ClassContent] - Check mime type for file element

---
+++ ClassContent/Traits/Element/FileMediaTrait.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+ * Copyright (c) 2017 Lp digital system
+ *
+ * This file is part of BackBee.
+ *
+ * BackBee is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BackBee is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with BackBee. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Charles Rouillon <charles.rouillon@lp-digital.fr>
+ */
+
+namespace BackBee\ClassContent\Traits\Element;
+
+use BackBee\ClassContent\Element\File;
+use BackBee\ClassContent\MediaInterface;
+
+/**
+ * Trait FileMedia
+ *
+ * @category    BackBee
+ * @copyright   ©2017 - Lp digital system
+ * @author      Djoudi Bensid <djoudi.bensid@lp-digital.fr>
+ */
+trait FileMediaTrait
+{
+    /**
+     * @see MediaInterface::mimeTypeSupported()
+     */
+    public function mimeTypeSupported($mimeType)
+    {
+        if ($this instanceof File) {
+            $supported = $this->getProperty('mime-types-supported');
+            return ($supported ? in_array($mimeType, $supported) : true);
+        }
+
+        return false;
+    }
+}
